bfffed95a0c39002eacb20613dfad07d9f902709
Samuel Nygaard <snpe12@student.aau.dk>
2018-01-24 03:37:48 +0100

    (0.2.1) - gmtOffsets + country + guess
    - Added GMT offset option
    - Added country support (2-way)
    - Adding support for guessing timezone

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
+import { TimezonePickerService } from './timezone-picker.service';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimezonePickerComponent } from './timezone-picker.component';
 
 export * from './timezone-picker.component';
+export * from './timezone-picker.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    TimezonePickerComponent
-  ],
-  exports: [
-    TimezonePickerComponent
-  ]
+  imports: [CommonModule],
+  declarations: [TimezonePickerComponent],
+  exports: [TimezonePickerComponent]
 })
 export class TimezonePickerModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: TimezonePickerModule
+      ngModule: TimezonePickerModule,
+      providers: [TimezonePickerService]
     };
   }
 }
