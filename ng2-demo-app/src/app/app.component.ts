5cc261337252bd9b014115814d4c3d131ca2590f
Samuel Nygaard <snpe12@student.aau.dk>
2018-01-24 04:06:48 +0100

    (0.2.1]

--- ng2-demo-app/src/app/app.component.ts
+++ ng2-demo-app/src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
+import { TimezonePickerService } from 'ng2-timezone-selector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // viewProviders: [TimezonePickerService]
 })
 export class AppComponent {
+  country;
   timezone: string = null;
+  timezone1: string = null;
   disabled = false;
+  disabledOffset = false;
   placeholderString = 'Choose a timezone';
+
+  constructor() {}
+
   onChange(t: string) {
     console.log(t);
   }
   inputChange(e) {
-debugger;    
     this.placeholderString = e;
   }
 }
--- ng2-demo-app/src/app/app.module.ts
+++ ng2-demo-app/src/app/app.module.ts
@@ -2,21 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
-import { TimezonePickerModule } from 'ng2-timezone-selector';
+// import { TimezonePickerModule } from '../../../../ng2-timezone-selector/dist';
+import {
+  TimezonePickerModule,
+  TimezonePickerService
+} from 'ng2-timezone-selector';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
-    TimezonePickerModule
+    FormsModule
+    // Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
+    // TimezonePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
